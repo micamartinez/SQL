0974a804886a1b89a6064fc074cf37b7b396ac11
Mica <92948781+micamartinez@users.noreply.github.com>
2021-11-27 05:08:55 -0300

    Update 02_Teradata.sql
    new exercises solved

--- Teradata/02_Teradata.sql
+++ Teradata/02_Teradata.sql
@@ -15,68 +15,68 @@ GROUP BY sku;
 -- (a) Determine how many distinct skus there are in pairs of the skuinfo, skstinfo, and trnsact tables:
 SELECT COUNT(DISTINCT a.sku) 
 FROM skuinfo a 
-JOIN skstinfo b 
- ON a.sku=b.sku;
+ JOIN skstinfo b 
+  ON a.sku=b.sku;
 
 SELECT COUNT(DISTINCT a.sku) 
 FROM skuinfo a 
-JOIN trnsact b 
- ON a.sku=b.sku;
+ JOIN trnsact b 
+  ON a.sku=b.sku;
 
 SELECT COUNT(DISTINCT a.sku) 
 FROM skstinfo a 
-JOIN trnsact b 
- ON a.sku=b.sku;
+ JOIN trnsact b 
+  ON a.sku=b.sku;
 
 -- (b) Which skus are common to the three tables? 
 SELECT DISTINCT a.sku
 FROM skuinfo a
-JOIN skstinfo b
- ON a.sku = b.sku
-JOIN trnsact c
- ON b.sku = c.sku;
+ JOIN skstinfo b
+  ON a.sku = b.sku
+ JOIN trnsact c
+  ON b.sku = c.sku;
 
 -- (c) Which skus are common to pairs of tables?
 SELECT DISTINCT a.sku 
 FROM skuinfo a 
-JOIN skstinfo b 
- ON a.sku=b.sku;
+ JOIN skstinfo b 
+  ON a.sku=b.sku;
 
 SELECT DISTINCT a.sku 
 FROM skuinfo a 
-JOIN trnsact b 
- ON a.sku=b.sku;
+ JOIN trnsact b 
+  ON a.sku=b.sku;
 
 SELECT DISTINCT a.sku 
 FROM skstinfo a 
-JOIN trnsact b 
- ON a.sku=b.sku;
+ JOIN trnsact b 
+  ON a.sku=b.sku;
 
 -- (d) Which skus are unique to specific tables?
 -- unique skus in skuinfo table
 SELECT a.sku, b.sku, c.sku
 FROM skuinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.sku = b.sku
-LEFT JOIN trnsact c 
+ LEFT JOIN trnsact c 
   ON a.sku=c.sku
 WHERE b.sku IS NULL AND c.sku IS NULL;
 
 -- unique skus in skstinfo table
 SELECT a.sku, b.sku, c.sku
 FROM skuinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.sku = b.sku
-LEFT JOIN trnsact c 
+ LEFT JOIN trnsact c 
   ON a.sku=c.sku
 WHERE a.sku IS NULL AND c.sku IS NULL;
 
 -- unique skus in trnsact table
 SELECT a.sku, b.sku, c.sku
 FROM skuinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.sku = b.sku
-LEFT JOIN trnsact c 
+ LEFT JOIN trnsact c 
   ON a.sku=c.sku
 WHERE a.sku IS NULL AND b.sku IS NULL;
 
@@ -109,44 +109,44 @@ FROM trnsact;
 -- (b) Which stores are common to all four tables?
 SELECT a.store, b.store, c.store, d.store 
 FROM strinfo a 
-JOIN skstinfo b
- ON a.store = b.store
-JOIN store_msa c 
- ON c.store = a.store
-JOIN trnsact d 
- ON c.store = d.store;
+ JOIN skstinfo b
+  ON a.store = b.store
+ JOIN store_msa c 
+  ON c.store = a.store
+ JOIN trnsact d 
+  ON c.store = d.store;
  
  -- (c) Which stores are unique to specific tables?
 -- unique stores in strinfo table
 SELECT a.store, b.store, c.store, d.store
 FROM strinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.store = b.store
-LEFT JOIN store_msa c 
+ LEFT JOIN store_msa c 
   ON a.store = c.store
-LEFT JOIN trnsact d 
+ LEFT JOIN trnsact d 
   ON  d.store = c.store
 WHERE b.store IS NULL AND c.store IS NULL AND d.store IS NULL;
 
 -- unique stores in skstinfo table
 SELECT a.store, b.store, c.store, d.store
 FROM strinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.store = b.store
-LEFT JOIN store_msa c 
+ LEFT JOIN store_msa c 
   ON a.store = c.store
-LEFT JOIN trnsact d 
+ LEFT JOIN trnsact d 
   ON  d.store = c.store
 WHERE a.store IS NULL AND c.store IS NULL AND d.store IS NULL;
 
 -- unique stores in store_msa table
 SELECT a.store, b.store, c.store, d.store
 FROM strinfo a
-LEFT JOIN skstinfo b
+ LEFT JOIN skstinfo b
   ON a.store = b.store
-LEFT JOIN store_msa c 
+ LEFT JOIN store_msa c 
   ON a.store = c.store
-LEFT JOIN trnsact d 
+ LEFT JOIN trnsact d 
   ON  d.store = c.store
 WHERE a.store IS NULL AND b.store IS NULL AND d.store IS NULL;
 
@@ -187,25 +187,98 @@ FROM trnsact t
   ON s.sku = t.sku AND s.store = t.store
 WHERE t.stype = 'P' AND register = '640';
 
+
 -- Exercise 5 --
 -- On what day was the total value (in $) of returned goods the greatest?
+SELECT TOP 1 saledate, SUM(amt) 
+FROM trnsact
+WHERE stype = 'R'
+GROUP BY saledate
+ORDER BY SUM(amt) DESC;
+
+-- On what day was the total number of individual returned items the greatest? 
+SELECT TOP 1 saledate, SUM(quantity) 
+FROM trnsact
+WHERE stype = 'R'
+GROUP BY saledate
+ORDER BY SUM(quantity) DESC;
 
 
+-- Exercise 6 --
+-- What is the maximum price paid for an item in our database?
+SELECT MAX(orgprice)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MAX(sprice)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MAX(amt/quantity)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MAX(retail)
+FROM skstinfo;
+
+-- What is the minimum price paid for an item in our database? 
+SELECT MIN(orgprice)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MIN(sprice)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MIN(amt/quantity)
+FROM trnsact 
+WHERE stype = 'P';
+
+SELECT MIN(retail)
+FROM skstinfo;
 
 
+-- Exercise 7 --
+-- How many departments have more than 100 brands associated with them, and what are their descriptions? 
+SELECT s.dept, d.deptdesc, COUNT(DISTINCT s.brand) 
+FROM skuinfo s 
+ LEFT JOIN deptinfo d 
+  ON s.dept = d.dept
+GROUP BY s.dept, d.deptdesc
+HAVING COUNT(DISTINCT s.brand) > 100;
+
+-- Exercise 8 --
+-- Write a query that retrieves the department descriptions of each of the skus in the skstinfo table. 
+SELECT a.sku, c.deptdesc
+FROM skstinfo a
+ LEFT JOIN skuinfo b
+  ON a.sku = b.sku
+ LEFT JOIN deptinfo c
+  ON b.dept = c.dept 
+GROUP BY a.sku, c.deptdesc;
 
 
+-- Exercise 9 --
+-- What department (with department description), brand, style, and color had the greatest total value of returned items? 
+SELECT TOP 1 d.dept, d.deptdesc, 
+s.brand, s.style, s.color,
+SUM(t.amt) 
+FROM deptinfo d
+ LEFT JOIN skuinfo s
+  ON d.dept = s.dept
+ LEFT JOIN trnsact t
+  ON s.sku = t.sku
+WHERE t.stype = 'R'
+GROUP BY d.dept, d.deptdesc, s.brand, s.style, s.color
+ORDER BY SUM(t.amt) DESC;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+-- Exercise 10 --
+-- In what state, city and zip code is the store that had the greatest total revenue during the time period monitored in our dataset? 
+SELECT TOP 1 s.state, s.city, s.zip, SUM(t.amt)
+FROM strinfo s
+ LEFT JOIN trnsact t
+  ON s.store = t.store
+WHERE t.stype = 'P'
+GROUP BY s.state, s.city, s.zip
+ORDER BY SUM(t.amt) DESC;
